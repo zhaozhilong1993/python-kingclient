c671649dca23750947074ead687c01d87adc17bf
466539725@qq.com <root@controller.novalocal>
2016-06-17 22:57:03 +0800

    Create a basic framework for kingclinet.
    Create a basic framework for kingclinet.

---
+++ kingclient/v1/shell.py
@@ -0,0 +1,44 @@
+# Copyright 2012 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from oslo_serialization import jsonutils
+from oslo_utils import strutils
+import six
+from six.moves.urllib import request
+import sys
+import yaml
+
+from kingclient.common import utils
+
+from kingclient.openstack.common._i18n import _
+from kingclient.openstack.common._i18n import _LI
+from kingclient.openstack.common._i18n import _LW
+
+import kingclient.exc as exc
+
+logger = logging.getLogger(__name__)
+
+
+def do_service_list(hc, args=None):
+    '''List the Heat engines.'''
+    show_deprecated('king service-list',
+                    'openstack orchestration service list')
+
+    fields = ['hostname', 'binary', 'engine_id', 'host',
+              'topic', 'updated_at', 'status']
+    services = hc.services.list()
+    utils.print_list(services, fields, sortby_index=1)
